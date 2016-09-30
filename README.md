270baff7631a822647ca9a3ebb103253f8a87c2c
José Manuel <jose.m1995@hotmail.com>
2016-09-30 17:55:29 +0200

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# repositorio con los ejercicios de clse de PHP
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php 
+echo"<pre>";
+//var_dump($_SERVER);
+echo "</pre>";
+echo "el método de la peticion es ".$_SERVER['REQUEST_METHOD'];
+?>
